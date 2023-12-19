75b3e5a5b8324bb8701fa7c2fd4d4bb72e8f43b9
Eldor <eldor.sherov.97@inbox.ru>
2023-12-19 14:33:17 +0300

    fixed templates and styles

---
+++ app/static/js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let code_block = document.querySelector('.post__content pre')
+  code_block.classList.add('post__content--code', 'code__content')
+});
